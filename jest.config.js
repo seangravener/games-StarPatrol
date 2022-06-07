ab0250f69b17c886b92a53e63613c696498d1c99
Sean Gravener <sean@gravener.net>
2022-06-07 07:48:04 -0400

    Setup DI lib (InversionJS)

--- jest.config.js
+++ jest.config.js
@@ -1,43 +1,22 @@
 /*
- * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/en/configuration.html
  */
 
 module.exports = {
-  // All imported modules in your tests should be mocked automatically
-  // automock: false,
-
-  // Stop running tests after `n` failures
-  // bail: 0,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/private/var/folders/st/q5vst2xj7zg0f9m96dc977rc0000gn/T/jest_dx",
-
-  // Automatically clear mock calls and instances between every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-
-  // An array of regexp pattern strings used to skip coverage collection
   coveragePathIgnorePatterns: ['/node_modules/', '/.git/'],
-
-  // Indicates which provider should be used to instrument code for coverage
   coverageProvider: 'babel',
 
-  // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  // setupFiles: [    "jest-canvas-mock"  ],
+
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
